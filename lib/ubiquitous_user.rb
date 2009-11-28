d4e20fcf92f50a325714ce14e99e8a43ddcffb4d
J. Pablo Fernández <pupeno@pupeno.com>
2009-11-28 23:37:42 +0100

    Remove the configurability, it didn't work.

--- lib/ubiquitous_user.rb
+++ lib/ubiquitous_user.rb
@@ -1,32 +1,24 @@
-
-module Usable
-  UserModel = User
-  UserModelSave = :save
-end
-
 module UsableHelpers
-  Users = {}
-  
   # Helper method to get the current user. It will always return a user but the
   # user may not be in the database. If options[:create] is true, then the user
   # will be in the database (although it may be a ghost user).
   def user(options = {:create => false})
     # If we already have a user object, return that.
-    return Users[object_id] if Users[object_id] != nil
+    return @ubiquitous_user if @ubiquitous_user != nil
     
     # Try to find the user in the database if session[:user_id] is defined.
-    Users[object_id] = User.find(session[:user_id]) if session[:user_id] != nil
-    return Users[object_id] if Users[object_id] != nil
+    @ubiquitous_user = User.find(session[:user_id]) if session[:user_id] != nil
+    return @ubiquitous_user if @ubiquitous_user != nil
     
     # Create a new user object.
-    Users[object_id] = Usable::UserModel.new()
+    @ubiquitous_user = User.new()
     if options[:create]
       # Save the user in the database and set the session user_id for latter.
-      # TODO use Usable::UserModelSave
-      Users[object_id].save_bypassing_non_essential_validation
-      session[:user_id] = Users[object_id].id
+      # TODO use UsableConfig::UserModelSave
+      @ubiquitous_user.save_bypassing_non_essential_validation
+      session[:user_id] = @ubiquitous_user.id
     end
-    return Users[object_id]
+    return @ubiquitous_user
   end
   
   # Helper method to get a user that for sure exists on the database.
@@ -42,14 +34,14 @@ end
 module Usable
   include UsableHelpers
   
-  def user=(user)
-    session[:user_id] = user and user.id or nil
-    session[:user_name] = user and user.name or nil
+  def user=(u)
+    session[:user_id] = u and u.id or nil
+    session[:user_name] = u and u.name or nil
     user
   end
   
   def authorize
-    unless UserModel.find_by_id(session[:user_id]) and session[:user_name] != nil
+    unless User.find_by_id(session[:user_id]) and session[:user_name] != nil
       flash[:notice] = "Please log in."
       redirect_to new_session_url
     end
