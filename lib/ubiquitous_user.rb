33e57fe432f31ea7f0ac2f604f851489b455ddd2
J. Pablo Fernández <pupeno@pupeno.com>
2009-11-29 14:45:34 +0100

    Added more configurability.

--- lib/ubiquitous_user.rb
+++ lib/ubiquitous_user.rb
@@ -1,13 +1,26 @@
 module UsableConfig
+  @user_model = :User
+  @user_model_new = :new
   @user_model_save = :save
   @user_model_name = :name
   
+  # Class that defines the user model.
+  attr_accessor :user_model
+  module_function :user_model, :user_model=
+  # Method used to create a new user, of class user_model
+  attr_accessor :user_model_new
+  module_function :user_model_new, :user_model_new=
   # Method used to save the user.
   attr_accessor :user_model_save
+  module_function :user_model_save, :user_model_save=
   # Method used to get the name of the user.
   attr_accessor :user_model_name
-  
-  module_function :user_model_save, :user_model_save=, :user_model_name, :user_model_name=
+  module_function :user_model_name, :user_model_name=
+
+  def user_model_class  # :nodoc:
+    Object.const_get(user_model)
+  end
+  module_function :user_model_class
 end
 
 module UsableHelpers
@@ -19,11 +32,11 @@ module UsableHelpers
     return @ubiquitous_user if @ubiquitous_user != nil
     
     # Try to find the user in the database if session[:user_id] is defined.
-    @ubiquitous_user = User.find(session[:user_id]) if session[:user_id] != nil
+    @ubiquitous_user = UsableConfig::user_model_class.find(session[:user_id]) if session[:user_id] != nil
     return @ubiquitous_user if @ubiquitous_user != nil
     
     # Create a new user object.
-    @ubiquitous_user = User.new()
+    @ubiquitous_user = UsableConfig::user_model_class.send(UsableConfig::user_model_new)
     if options[:create]
       # Save the user in the database and set the session user_id for latter.
       @ubiquitous_user.send(UsableConfig::user_model_save)
@@ -52,7 +65,7 @@ module Usable
   end
   
   def authorize
-    unless User.find_by_id(session[:user_id]) and session[:user_name] != nil
+    unless UsableConfig::user_model_class.find_by_id(session[:user_id]) and session[:user_name] != nil
       flash[:notice] = "Please log in."
       redirect_to new_session_url
     end
